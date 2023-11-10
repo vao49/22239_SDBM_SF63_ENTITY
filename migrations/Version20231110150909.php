3df6b6898703f99814df14b80c01547efdf97f49
vao49 <fbrc.le.blanc@gmail.com>
2023-11-10 16:11:51 +0100

    Ticket avec ID

---
+++ migrations/Version20231110150909.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231110150909 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ticket ADD id INT AUTO_INCREMENT NOT NULL, DROP PRIMARY KEY, ADD PRIMARY KEY (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ticket MODIFY id INT NOT NULL');
+        $this->addSql('DROP INDEX `PRIMARY` ON ticket');
+        $this->addSql('ALTER TABLE ticket DROP id');
+        $this->addSql('ALTER TABLE ticket ADD PRIMARY KEY (annee, numero_ticket)');
+    }
+}
--- src/Entity/Ticket.php
+++ src/Entity/Ticket.php
@@ -3,34 +3,30 @@
 namespace App\Entity;
 
 use App\Repository\TicketRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TicketRepository::class)]
 class Ticket
 {
-    // #[ORM\Id]
-    // #[ORM\GeneratedValue]
-    // #[ORM\Column]
-    // private ?int $id = null;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
 
     #[ORM\Column]
-    #[ORM\Id]
     private ?int $annee = null;
 
     #[ORM\Column]
-    #[ORM\Id]
     private ?int $numeroTicket = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $dateVente = null;
 
-    // public function getId(): ?int
-    // {
-    //     return $this->id;
-    // }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
     public function getAnnee(): ?int
     {
